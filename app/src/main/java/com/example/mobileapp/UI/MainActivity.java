97320d4fa8633086c9f213777b6f5479a6959e71
River Halverson <rhalve3@student.wgu.edu>
2023-07-13 10:56:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mobileapp/UI/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mobileapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mobileapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button termButton = findViewById(R.id.termsbutton);
+        termButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TermsList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button courseButton = findViewById(R.id.coursesbutton);
+        courseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CoursesList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button assessmentsButton = findViewById(R.id.assessmentsbutton);
+        assessmentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AssessmentsList.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
+}
